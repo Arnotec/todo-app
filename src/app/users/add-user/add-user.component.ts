a978f4438303f2a92873cf459b4a8aa81d5a5fb9
Arnaud <tsofackarnaud@gmail.com>
2021-06-26 13:52:48 +0100

    Formulaires : reactive meth -  recup des donnees saisies

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup | any;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initUserForm();
+  }
+
+  initUserForm(){
+    this.userForm = this.formBuilder.group({
+      firstName: this.formBuilder.control(""),
+      lastName: this.formBuilder.control(""),
+      email: this.formBuilder.control(""),
+      description: this.formBuilder.control(""),
+      dateBirth: this.formBuilder.control(""),
+      address: this.formBuilder.group({
+        street: this.formBuilder.control(""),
+        state: this.formBuilder.control(""),
+        zip: this.formBuilder.control(""),
+        city: this.formBuilder.control("")
+      })
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.userForm.value);
+
   }
 
 }
